9926b59370156074502ab5fe1cd2eed7bf21885a
Liliana Tom <karenliliguancha@unicauca.edu.co>
2019-03-26 17:30:38 -0500

    krn

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10 ">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Codigo del Producto</label>
+                    <input class="form-control" type="text" placeholder="Codigo del producto a redimir">
+                </div>                       
+                <button type="submit" class="btn btn-primary">Buscar</button>
+            </form>
+        </div>    
+    </div>
+</div>
